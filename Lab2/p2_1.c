5cc21870b9fd37f19ffa14e88324ce0aac3791bc
2016025305 <2016025305@hanyang.ac.kr>
2018-03-15 17:21:23 +0900

    p2_1완료 / Makefile 생성

---
+++ Lab2/p2_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void swap(int *num1, int *num2)
+
+int main(int argc, char **argv) {
+	int num1, num2;
+	printf("Enter two integers: ");
+	scanf("%d %d", &num1, &num2);
+	swap(&num1, &num2);
+
