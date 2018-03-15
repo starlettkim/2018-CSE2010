6c6fd6fe370c672b3b3e73842068b82aacdeda6c
2016025305 <2016025305@hanyang.ac.kr>
2018-03-15 17:22:57 +0900

    ??

--- Lab2/p2_1.c
+++ Lab2/p2_1.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-void swap(int *num1, int *num2)
+void swap(int *num1, int *num2) {
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
 
 int main(int argc, char **argv) {
 	int num1, num2;
 	printf("Enter two integers: ");
 	scanf("%d %d", &num1, &num2);
-	swap(&num1, &num2);
 
+	swap(&num1, &num2);
+	printf("Swapped: %d %d\n", num1, num2);
+	return 0;
+}
